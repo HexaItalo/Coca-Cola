b5dcc2acb1be0b1d8cfb07508d3195563ec22e88
LoiusBorges <loiusborges@gmail.com>
2024-10-30 13:07:10 -0300

    Navegação

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,23 @@
 const menu = document.querySelector(".menu");
 const menuIcon = document.getElementById("menu-icon");
+const navBar = document.querySelector(".nav-bar");
 
 menu.onclick = function () {
-    const navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("ativar");
+    if (window.innerWidth <= 1000) { 
+        navBar.classList.toggle("ativar");
+        
+        if (navBar.classList.contains("ativar")) {
+            menuIcon.src = "/src/icons/icons-fechar.svg"; 
+        } else {
+            menuIcon.src = "/src/icons/icons-menu.svg"; 
+        }
+    }
+};
 
-    
-    if (navBar.classList.contains("ativar")) {
-        menuIcon.src = "/src/icons/icons-fechar.svg"; 
-    } else {
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 1000) {
+        navBar.classList.remove("ativar"); 
         menuIcon.src = "/src/icons/icons-menu.svg"; 
     }
-}
+});
